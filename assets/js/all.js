302297aa3d765d5cfe5f629bd8eb66516bd7cb81
yachuh <yachuh@gmail.com>
2023-04-13 13:29:21 +0800

    Update 2023-04-13T05:29:19.728Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  console.log("Hello Bootstrap5");
+});
+ClassicEditor.create(document.querySelector("#editor"))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
