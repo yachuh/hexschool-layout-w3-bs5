25b4f01ee2fa3cd229e0b410d62cabb70d6f6084
yachuh <yachuh@gmail.com>
2023-04-13 13:30:19 +0800

    Added collapse content to index

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,7 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(function () {
+  console.log("Hello Bootstrap5");
+});
+
+ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
+  console.error(error);
 });
