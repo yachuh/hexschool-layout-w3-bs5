4691d1a0b6a4a9b5cbeb082708b5e2ed2156041a
yachuh <yachuh@gmail.com>
2023-04-14 16:54:02 +0800

    Update 2023-04-14T08:54:00.785Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,28 @@
 "use strict";
 
-$(function () {
-  console.log("Hello Bootstrap5");
-});
+//=== Nav Active Style ===//
+var navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    var currentActive = document.querySelector(".active");
+
+    if (currentActive) {
+      currentActive.classList.remove("active");
+    }
+
+    this.classList.add("active");
+  });
+}); // Set the active class for the current page URL
+
+var currentUrl = window.location.href;
+
+for (var i = 0; i < navLinks.length; i++) {
+  if (navLinks[i].href === currentUrl) {
+    navLinks[i].classList.add("active");
+  }
+} //=== CK Editor JS plugin ===//
+
+
 ClassicEditor.create(document.querySelector("#editor"))["catch"](function (error) {
   console.error(error);
 });
